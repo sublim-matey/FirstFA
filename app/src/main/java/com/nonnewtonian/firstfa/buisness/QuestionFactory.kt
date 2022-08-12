4f8655448d86451bf51d47f460f1e96e2ede1e9d
Max H <maxfieldbonafacio@gmail.com>
2022-08-11 20:17:33 -0700

    Improved QuestionFactory.kt

--- app/src/main/java/com/nonnewtonian/firstfa/buisness/QuestionFactory.kt
+++ app/src/main/java/com/nonnewtonian/firstfa/buisness/QuestionFactory.kt
@@ -5,15 +5,15 @@ import com.nonnewtonian.firstfa.model.TrainingType
 
 class QuestionFactory {
 
-    fun generateQuestion(trainingType: TrainingType): Question {
-        val inputs = generateInputs(0)
-        val wrongAnswers = generateWrongAnswers(trainingType, inputs, 0)
+    fun generateQuestion(trainingType: TrainingType, difficulty: Int = 0): Question {
+        val inputs = generateInputs(trainingType, difficulty)
+        val wrongAnswers = generateWrongAnswers(trainingType, inputs, difficulty)
         val correctAnswer = generateCorrectAnswer(trainingType, inputs)
-        return Question(trainingType,inputs, wrongAnswers, correctAnswer)
+        return Question(trainingType, inputs, wrongAnswers, correctAnswer)
     }
 
     private fun generateCorrectAnswer(trainingType: TrainingType, inputs: List<Int>): Int {
-        return when(trainingType) {
+        return when (trainingType) {
             TrainingType.Addition -> inputs[0] + inputs[1]
             TrainingType.Subtraction -> inputs[0] - inputs[1]
             TrainingType.Division -> inputs[0] / inputs[1]
@@ -21,38 +21,66 @@ class QuestionFactory {
         }
     }
 
-    private fun generateWrongAnswers(trainingType: TrainingType, inputs: List<Int>, difficulty: Int = 0): List<Int> {
-//        when {
-//            difficulty == 0 -> val ra = range(1..10)
-//        }
-//        TODO("assign random param generation based off difficulty")
-        val a = List(3) {
-            if(it % 2 == 0 && inputs[0] > 2){
-                inputs[0] - (1 until inputs[0]).random()
-            }
-             else if(difficulty < 3) {
-                inputs[0] / (1..2).random() + (1..10).random()
-            } else {
-                inputs[0] / (1..2).random() + (1..30).random()
-            }
+    private fun generateWrongAnswers(
+        trainingType: TrainingType,
+        inputs: List<Int>,
+        difficulty: Int = 0
+    ): List<Int> {
+
+        val wrongAns = mutableSetOf<Int>()
+        while (wrongAns.size < 3) {
+            val a =
+                if (difficulty < 3) {
+                    inputs[0] / (1..2).random() + (1..10).random()
+                } else {
+                    inputs[0] / (1..2).random() + (1..30).random()
+                }
+            wrongAns.add(a)
         }
-        return when(trainingType) {
-            TrainingType.Multiplication -> a.map {it * inputs[1]}
-            TrainingType.Addition -> a.map {it + inputs[1]}
-            TrainingType.Division -> a.map {it / inputs[1]}
-            TrainingType.Subtraction -> a.map { it - inputs[1] }
+        return when (trainingType) {
+            TrainingType.Multiplication -> wrongAns.map { it * inputs[1] }
+            TrainingType.Addition -> wrongAns.map { it + inputs[1] }
+            TrainingType.Division -> wrongAns.map { it / inputs[1] }
+            TrainingType.Subtraction -> wrongAns.map { it - inputs[1] }
         }
     }
 
-    private fun generateInputs(difficulty: Int = 0): List<Int> {
+    private fun generateInputs( trainingType: TrainingType, difficulty: Int = 0): List<Int> {
         //TODO("create difficulty adjustment")
 //        TODO("fix difficulty bug at low levels, to assure no doubled answers")
-        return when (difficulty) {
-            0 -> List(2) { (1..10).random() }
-            1 -> List(2) { (1..20).random() }
-            2 -> List(2) { (1..30).random() }
-            3 -> List(2) { (10..40).random() }
-            else -> List(2) { (20..9000).random() }
+        when (trainingType) {
+            TrainingType.Multiplication ->
+                return when (difficulty) {
+                    0 -> List(2) { (1..10).random() }
+                    1 -> List(2) { (1..20).random() }
+                    2 -> List(2) { (1..30).random() }
+                    3 -> List(2) { (10..40).random() }
+                    else -> List(2) { (20..9000).random() }
+                }
+            TrainingType.Division ->
+                return when (difficulty) {
+                    0 -> List(2) { (1..10).random() }
+                    1 -> List(2) { (1..20).random() }
+                    2 -> List(2) { (1..30).random() }
+                    3 -> List(2) { (10..40).random() }
+                    else -> List(2) { (20..9000).random() }
+                }
+            TrainingType.Addition ->
+                return when (difficulty) {
+                    0 -> List(2) { (1..10).random() }
+                    1 -> List(2) { (1..50).random() }
+                    2 -> List(2) { (1..100).random() }
+                    3 -> List(2) { (100..1000).random() }
+                    else -> List(2) { (100..9000).random() }
+                }
+            TrainingType.Subtraction ->
+                return when (difficulty) {
+                    0 -> List(2) { (1..10).random() }
+                    1 -> List(2) { (1..50).random() }
+                    2 -> List(2) { (1..100).random() }
+                    3 -> List(2) { (100..1000).random() }
+                    else -> List(2) { (100..9000).random() }
+                }
         }
     }
 }
